35977a959a0a04d4f39fd078d4ca65a998e52e33
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-07-15 08:57:18 -0400

    Finished core node app

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,14 +2,15 @@ const request = require('request')
 require('dotenv').config();
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=${latitude},${longitude}?units=f`
-  request({ url: url, json: true }, (error, response) => {
+  const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API}&query=${latitude},${longitude}&units=f`
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service!', undefined)
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-      callback(undefined, response.body.current.weather_descriptions[0] + ' It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + '% chance of rain.')
+      // callback(undefined, body)
+      callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
     }
   })
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -4,19 +4,19 @@ require('dotenv').config();
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.GEOCODE_API_KEY}`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to the location services!', undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback('Unable to find the location. Try another search.', undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        place: response.body.features[0].place_name
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
       });
     }
   });
 };
 
-module.exports = geocode
+module.exports = geocode;
